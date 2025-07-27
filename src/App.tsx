a1c4e3ce27e8de5a4920c955bad2b3a953e45512
nitintomar-jarvis <nitin.tomar@jarvis.consulting>
2025-07-28 05:02:01 +0530

    intial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import { Process } from './components/ui/card-stack-use'
+import HeroSection from './components/ui/hero-section-1'
+import { PremiumContact } from './components/ui/premium-contact'
+
+function App() {
+
+  return (
+    <>
+    <HeroSection/>
+    <Process/>
+    <PremiumContact/>
+    </>
+  )
+}
+
+export default App
